c05b36f26ca8c0bbd57cee913711142797102e9a
halfrice <halfrice@pm.me>
2019-10-26 14:29:50 -0700

    Styled components

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,18 @@
 import React from "react"
 import PropTypes from "prop-types"
+import styled from "styled-components"
 import Seo from "./seo"
+
+const Root = styled.div`
+  min-height: 100vh;
+`
 
 const Layout = ({ children }) => {
   return (
-    <div id="root">
+    <Root id="root">
       <Seo />
       <div>{children}</div>
-    </div>
+    </Root>
   )
 }
 
