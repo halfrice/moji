caed037369737f4ea9945ec6eb74f99e4130eb83
halfrice <halfrice@pm.me>
2019-11-17 13:36:34 -0800

    Emoji-mart

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,8 @@
 import React from "react"
 import styled from "styled-components"
+import "emoji-mart/css/emoji-mart.css"
+import data from "emoji-mart/data/messenger.json"
+import { NimblePicker, Emoji } from "emoji-mart"
 
 const AppContainer = styled.div`
   display: flex;
@@ -9,22 +12,42 @@ const AppContainer = styled.div`
   max-width: 1440px;
   padding: 20px;
 `
+const EmojiContainer = styled.div``
 const MessageTextArea = styled.textarea`
   width: 100%;
-  max-width: 500px;
-  min-height: 100px;
-`
-const SendButton = styled.button`
+  border: 1px solid #a4bdde;
+  border-radius: 6px;
+  max-width: 600px;
+  min-height: 150px;
   margin-top: 20px;
+  background-color: transparent;
+  font-size: 24px;
+  color: #a4bdde;
 `
-const CurrentMessage = styled.div`
-  margin-top: 20px;
+const PickerContainer = styled.div`
+  padding: 0px;
+`
+const SendButton = styled.div`
+  border: 1px solid yellowgreen;
+  border-radius: 6px;
+  margin: 20px 0;
+  padding: 0.75rem 1.25rem;
+  background-color: transparent;
+  line-height: 1;
+  text-decoration: none;
+  cursor: pointer;
+  &:hover,
+  &:focus,
+  &:active {
+    background-color: #404144;
+  }
 `
 
 class App extends React.Component {
   state = {
     textAreaValue: "",
     message: "",
+    selectedEmoji: "grin",
   }
 
   handleTextAreaChange = event => {
@@ -32,21 +55,49 @@ class App extends React.Component {
   }
 
   sendMessage = () => {
-    this.setState({ message: this.state.textAreaValue })
+    this.setState({ message: this.state.textAreaValue }, () => {
+      console.log("Sending message: " + this.state.message)
+    })
+  }
+
+  handleEmojiSelect = emoji => {
+    this.setState({
+      selectedEmoji: emoji.short_names[0] ? emoji.short_names[0] : emoji.id,
+      textAreaValue: this.state.textAreaValue + emoji.native,
+    })
   }
 
   render() {
     return (
       <AppContainer>
-        <h2>App</h2>
+        <EmojiContainer>
+          <Emoji
+            emoji={this.state.selectedEmoji}
+            fallback={(emoji, props) => {
+              return emoji ? `:${emoji.short_names[0]}:` : props.emoji
+            }}
+            set={"apple"}
+            sheetSize={64}
+            size={256}
+          />
+        </EmojiContainer>
         <MessageTextArea
           type="text"
           onChange={this.handleTextAreaChange}
           value={this.state.textAreaValue}
           placeholder="Enter message"
         ></MessageTextArea>
-        <SendButton onClick={this.sendMessage}>Enter</SendButton>
-        <CurrentMessage>Sent Message: {this.state.message}</CurrentMessage>
+        <PickerContainer>
+          <NimblePicker
+            data={data}
+            emoji="grin"
+            onSelect={this.handleEmojiSelect}
+            set="apple"
+            sheetSize={64}
+            title="Moji"
+          />
+        </PickerContainer>
+        <SendButton onClick={this.sendMessage}>Send</SendButton>
       </AppContainer>
     )
   }
